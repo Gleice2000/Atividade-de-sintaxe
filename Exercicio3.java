c4d5b298be4a34ab0afe02f8ca088eb44a7b1fdc
Gleiceara Alves de Azevedo <gleiceara.alves@alunos.ifsuldeminas.edu.br>
2023-02-13 19:58:39 -0300

    Add files via upload

---
+++ Exercicio3.java
@@ -0,0 +1,12 @@
+//Imprima todos os múltiplos de 5, entre 1 e 100.
+public class Exercicio3 {
+    
+    public static void main(String[]args){
+	
+	for(int i=1 ; i<=100 ; i++){
+		if(i % 5 ==0){
+			System.out.println(i);
+		}
+}
+    }
+}
