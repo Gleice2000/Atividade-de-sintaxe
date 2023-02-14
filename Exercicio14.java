1a55e0708cc372bb986ab06a762cb9a5bb483083
Gleiceara Alves de Azevedo <gleiceara.alves@alunos.ifsuldeminas.edu.br>
2023-02-13 22:31:13 -0300

    Add files via upload

---
+++ Exercicio14.java
@@ -0,0 +1,17 @@
+//Utilizando o for
+
+public class Exercicio14{
+public static void main(String[] args) {
+	String x = "Amanda";
+
+	int total=0;
+
+	for (int i=0;i<=x.length()-1;i++) {
+		if (x.substring(i,i+1).equalsIgnoreCase("a"))
+			total=total+1;
+	}
+
+	System.out.println(total);
+
+}
+}
