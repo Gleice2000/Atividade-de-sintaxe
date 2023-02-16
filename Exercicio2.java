1a441ec6e58e0391d3272e97d921a4d4b659786c
Gleiceara Alves de Azevedo <gleiceara.alves@alunos.ifsuldeminas.edu.br>
2023-02-15 21:41:45 -0300

    Add files via upload

---
+++ Exercicio2.java
@@ -0,0 +1,12 @@
+//Imprima a soma de 1 até 100.
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+    int num =1;
+    int soma;
+    for(int a = 0; a<= 100; a++){
+        soma = num + a;
+        System.out.println(num + "+" + a + "=" + soma);
+    }
+    }
+}
